ed2f8c5dc4fa1b700ac397999869ed57645d3c92
Gautham <gauthamskaniyattil@gmail.com>
2025-01-26 21:38:33 +0530

    Tested out try catch instead of asynhandler

--- src/controllers/healthCheckController.js
+++ src/controllers/healthCheckController.js
@@ -1,10 +1,21 @@
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
 
-const healthcheck = asyncHandler(async (req, res) => {
-  return res
-    .status(200)
-    .json(new ApiResponse(200, "OK", "Health check passed"));
-});
+// const healthcheck = asyncHandler(async (req, res) => {
+//   return res
+//     .status(200)
+//     .json(new ApiResponse(200, "OK", "Health check passed"));
+// });
+
+const healthcheck = async (requestAnimationFrame, res) => {
+  try {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "OK!", "Health check passed"));
+  } catch (err) {
+    res.status(404).json(new ApiError(404, "Oops!!"));
+  }
+};
 
 export { healthcheck };
